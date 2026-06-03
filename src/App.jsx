58fb50c6e0994119fd23cb4119178df044b4d810
Omersteinberg <omersteinberg1204@gmail.com>
2026-06-03 17:58:52 +1000

    fix cases

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { initialPlayers }      from './Data/playersData';
+import { initialPlayers }      from './data/playersData';
 import BanterTicker             from './components/BanterTicker';
 import Leaderboard              from './components/Leaderboard';
 import LiveScores               from './components/LiveScores';
