1b953425910dc4fe43aa24f22ceab14e5723cf56
Omersteinberg <omersteinberg1204@gmail.com>
2026-06-04 19:06:23 +1000

    captial D

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useMemo } from 'react';
-import { initialPlayers }      from './data/playersdata';
+import { initialPlayers }      from './data/playersData';
 import PasswordGate             from './components/PasswordGate';
 import BanterTicker             from './components/BanterTicker';
 import Leaderboard              from './components/Leaderboard';
